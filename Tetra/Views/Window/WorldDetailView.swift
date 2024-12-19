9db60b0efbbe33ce4b93b12968e944a17d2ff188
yugoatobe <yugoatobe0330@gmail.com>
2024-12-19 22:02:01 +0900

    personacameraを追加

--- Tetra/Views/Window/WorldDetailView.swift
+++ Tetra/Views/Window/WorldDetailView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import GroupActivities
 
 /// A view that presents the video content details.
-struct DetailView: View {
+struct WorldDetailView: View {
     @State var inputSharePlayLink = ""
     
     let room: (
