b7f4e20a79adef19afc53d61ddf52ac8a493cc41
Seinosuke01 <seinosuke.saito520@gmail.com>
2024-12-31 18:31:39 +0900

    Check Relay

--- Tetra/Function/groupMetadata.swift
+++ Tetra/Function/groupMetadata.swift
@@ -2,15 +2,23 @@ import Foundation
 import Nostr
 
 func handleGroupMetadata(appState: AppState, event: Event) {
+    let targetGroupIds = [
+        "c39cbb08b200e6ef876af8b7f053510fa38b251b4789dac5525ea344e9df1908",
+        "39984c613072802df81cda7db0adc5e86edbe143eae5fadb38d8d3789f31b070"
+    ]
+    
     let tags = event.tags.map({ $0 })
-    guard let groupId = tags.first(where: { $0.id == "d" })?.otherInformation.first else { return }
-    let isPublic = tags.first(where: { $0.id == "private"}) == nil
+    guard let groupId = tags.first(where: { $0.id == "d" })?.otherInformation.first, targetGroupIds.contains(groupId) else {
+        return
+    }
+
+    let isPublic = tags.first(where: { $0.id == "private" }) == nil
     let isOpen = tags.first(where: { $0.id == "closed" }) == nil
     let name = tags.first(where: { $0.id == "name" })?.otherInformation.first
     let about = tags.first(where: { $0.id == "about" })?.otherInformation.first
     let picture = tags.first(where: { $0.id == "picture" })?.otherInformation.first
     let link = tags.first(where: { $0.id == "r" })?.otherInformation.first
-    
+
     let metadata = ChatGroupMetadata(
         id: groupId,
         relayUrl: appState.selectedNip29Relay?.url ?? "",
@@ -23,7 +31,7 @@ func handleGroupMetadata(appState: AppState, event: Event) {
         isAdmin: false,
         link: link
     )
-    
+
     DispatchQueue.main.async {
         appState.allChatGroup.append(metadata)
     }
