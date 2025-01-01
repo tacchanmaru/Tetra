824ad0738c5dd976031392b8214e098bf0d9c458
Seinosuke01 <seinosuke.saito520@gmail.com>
2025-01-01 11:16:52 +0900

    不要な改行の削除

--- Tetra/Function/groupMetadata.swift
+++ Tetra/Function/groupMetadata.swift
@@ -4,14 +4,12 @@ import Nostr
 func handleGroupMetadata(appState: AppState, event: Event) {
     let tags = event.tags.map({ $0 })
     guard let groupId = tags.first(where: { $0.id == "d" })?.otherInformation.first else { return }
-
     let isPublic = tags.first(where: { $0.id == "private" }) == nil
     let isOpen = tags.first(where: { $0.id == "closed" }) == nil
     let name = tags.first(where: { $0.id == "name" })?.otherInformation.first
     let about = tags.first(where: { $0.id == "about" })?.otherInformation.first
     let picture = tags.first(where: { $0.id == "picture" })?.otherInformation.first
     let link = tags.first(where: { $0.id == "r" })?.otherInformation.first
-
     let metadata = ChatGroupMetadata(
         id: groupId,
         relayUrl: appState.selectedNip29Relay?.url ?? "",
@@ -24,7 +22,6 @@ func handleGroupMetadata(appState: AppState, event: Event) {
         isAdmin: false,
         link: link
     )
-
     DispatchQueue.main.async {
         appState.allChatGroup.append(metadata)
     }
